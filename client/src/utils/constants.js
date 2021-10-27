227012fe22f866de87211c82e1f0ea6c283ac07b
Abhishek Ghosh <abhishekghosh018@gmail.com>
2021-10-27 12:39:42 +0530

    Added production service url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "http://localhost:4001/api";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://nexusweb.herokuapp.com/api"
+    : "http://localhost:4001/api";
 export const ORDER_BY = ["", "asc", "desc"];
 export const IMDB_API_KEY = ["k_35stsjw8", "k_8b5nf80f"];
