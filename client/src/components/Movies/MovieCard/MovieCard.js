392a508e8baacc6d158ea2e5e74599a5b77cb159
Abhishek <abhishekghosh018@gmail.com>
2021-12-14 21:14:17 +0530

    Filter

--- client/src/components/Movies/MovieCard/MovieCard.js
+++ client/src/components/Movies/MovieCard/MovieCard.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TruncatedElement from "../../common/TruncatedElement/TruncatedElement";
 import MovieAction from "../common/MovieAction/MovieAction";
 import RatingStar from "../common/RatingStar/RatingStar";
+import Filterable from "../MovieTable/Filterable/Filterable";
+import Sortable from "../MovieTable/Sortable/Sortable";
 
 import "./movieCard.scss";
 
@@ -20,99 +22,123 @@ function MovieCard({
   setUpdateBody,
   updateMovie,
 }) {
+  const [sourceFilter, setSourceFilter] = useState([]);
+
   return (
-    <div className="movie-card-wrapper">
-      {allMovies.map(
-        ({
-          _id: id,
-          imDbId,
-          image,
-          title,
-          year,
-          type,
-          imDb,
-          genres,
-          source,
-          watchQueue,
-          watched,
-          rewatchScore,
-        }) =>
-          watched === displayWatched && (
-            <section className="movie-card" key={imDbId}>
-              <div className="movie-details">
-                <img src={image} alt={`${title} poster`} />
-                <div className="movie-content">
-                  <p label={`${title} (${year})`} className="movie-name">
-                    {`${title} (${year})`}
-                  </p>
-                  <div>
-                    {editId === id ? (
-                      <select
-                        value={updateBody.source}
-                        onChange={(e) =>
-                          setUpdateBody((prev) => ({ ...prev, source: e.target.value }))
-                        }
-                      >
-                        {sourceList.map((option) => (
-                          <option key={option._id} value={option.name}>
-                            {option.name}
-                          </option>
-                        ))}
-                      </select>
-                    ) : (
-                      source || "-"
-                    )}
-                  </div>
-                  {/* <div className="extra-wrapper">
-                    <div className={editId === imDbId ? "movie-detail-open" : "movie-detail-close"}> */}
-                  <TruncatedElement label={genres.join(", ")} className="genres" />
-                  <div>Imdb: {imDb || "-"}</div>
-                  {displayWatched && (
+    <>
+      <div style={{ position: "relative" }}>
+        <div style={{ position: "absolute", bottom: 18, display: "flex", gap: 20 }}>
+          <Sortable
+            handleSortBy={setSortBy}
+            headerLabel="Rating"
+            field="imDb"
+            sortedName={sortBy.name}
+            sortedOrder={sortBy.order}
+          />
+          {/* <Filterable
+            headerLabel="Source"
+            filterableFields={sourceFilter}
+            handleFilterableFields={setSourceFilter}
+            filterOptions={sourceList}
+          /> */}
+        </div>
+      </div>
+
+      <div className="movie-card-wrapper">
+        {allMovies.map(
+          ({
+            _id: id,
+            imDbId,
+            image,
+            title,
+            year,
+            type,
+            imDb,
+            genres,
+            source,
+            watchQueue,
+            watched,
+            rewatchScore,
+          }) =>
+            watched === displayWatched && (
+              <section className="movie-card" key={imDbId}>
+                <div className="movie-details">
+                  <img src={image} alt={`${title} poster`} />
+                  <div className="movie-content">
                     <div>
-                      Rewatch:
-                      <RatingStar
-                        id={id}
-                        editId={editId}
-                        updateBody={updateBody}
-                        rewatchScore={rewatchScore}
-                        setUpdateBody={setUpdateBody}
-                      />
+                      <p label={`${title} (${year})`} className="movie-name">
+                        {`${title} (${year})`}
+                      </p>
                     </div>
-                  )}
-                  {!displayWatched && (
-                    <div>
-                      Queue:{" "}
+                    <div className="source">
                       {editId === id ? (
-                        <input
-                          name="watchQueue"
-                          value={updateBody.watchQueue}
+                        <select
+                          value={updateBody.source}
                           onChange={(e) =>
-                            setUpdateBody((prev) => ({ ...prev, watchQueue: e.target.value }))
+                            setUpdateBody((prev) => ({ ...prev, source: e.target.value }))
                           }
-                        />
+                        >
+                          {sourceList.map((option) => (
+                            <option key={option._id} value={option.name}>
+                              {option.name}
+                            </option>
+                          ))}
+                        </select>
                       ) : (
-                        watchQueue || "-"
+                        source || "-"
                       )}
                     </div>
-                  )}
+                    {/* <div className="extra-wrapper">
+                      <div className={editId === imDbId ? "movie-detail-open" : "movie-detail-close"}> */}
+                    <TruncatedElement label={genres.join(", ")} className="genres" />
+                    <div>Imdb: {imDb || "-"}</div>
+                    {displayWatched && (
+                      <div>
+                        Rewatch:
+                        <RatingStar
+                          id={id}
+                          editId={editId}
+                          updateBody={updateBody}
+                          rewatchScore={rewatchScore}
+                          setUpdateBody={setUpdateBody}
+                        />
+                      </div>
+                    )}
+                    {!displayWatched && (
+                      <div>
+                        Queue:{" "}
+                        {editId === id ? (
+                          <input
+                            name="watchQueue"
+                            value={updateBody.watchQueue}
+                            onChange={(e) =>
+                              setUpdateBody((prev) => ({ ...prev, watchQueue: e.target.value }))
+                            }
+                          />
+                        ) : (
+                          watchQueue || "-"
+                        )}
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <MovieAction
-                id={id}
-                setId={setEditId}
-                isCurrentId={editId === id}
-                updateBody={() => setUpdateBody({ source, watchQueue, rewatchScore })}
-                reset={() => {
-                  setEditId(null);
-                  setUpdateBody({});
-                }}
-                watched={watched}
-                updateMovie={updateMovie}
-              />
-            </section>
-          )
-      )}
-    </div>
+                <MovieAction
+                  id={id}
+                  setId={setEditId}
+                  isCurrentId={editId === id}
+                  updateBody={() => setUpdateBody({ source, watchQueue, rewatchScore })}
+                  reset={() => {
+                    setEditId(null);
+                    setUpdateBody({});
+                  }}
+                  watched={watched}
+                  updateMovie={updateMovie}
+                />
+              </section>
+            )
+        )}
+      </div>
+    </>
   );
 }
 
