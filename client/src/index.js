bf1136f7b947364270992df4bf1661cfc0e2a316
Abhishek <abhishekghosh018@gmail.com>
2022-01-10 16:38:39 +0530

    Add hashrouter and favicon

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
-import App from "./App";
+import { HashRouter } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
 import { GlobalProvider } from "./context/GlobalState";
 
+import App from "./App";
+
+import "./index.scss";
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalProvider>
         <App />
       </GlobalProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
