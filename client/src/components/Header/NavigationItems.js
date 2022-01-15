d2f2f76968768fbc7839f1a6e3c7c5b89a6605ee
Abhishek <abhishekghosh018@gmail.com>
2022-01-15 23:28:02 +0530

    Resume code added

--- client/src/components/Header/NavigationItems.js
+++ client/src/components/Header/NavigationItems.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalState";
 
 import ROUTES from "../../routes.json";
 
 function NavigationItems() {
+  let {
+    users: {
+      userDetails: { isAdmin },
+    },
+  } = useContext(GlobalContext);
+
   return (
     <ul>
       {Object.keys(ROUTES).map((nav) => {
-        const { name, url } = ROUTES[nav];
+        const { name, url, isProtected } = ROUTES[nav];
 
         return (
-          <li key={name}>
-            <NavLink to={url} exact={true}>
-              {name}
-            </NavLink>
-          </li>
+          (!isProtected || isAdmin) && (
+            <li key={name}>
+              <NavLink to={url} exact={true}>
+                {name}
+              </NavLink>
+            </li>
+          )
         );
       })}
     </ul>
